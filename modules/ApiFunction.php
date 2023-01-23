ba203a73141918399d18b7e0ee6b599d95d4725b
XiaoLFeng <gm@x-lf.cn>
2023-01-23 18:25:14 +0800

    生成User_ssid

--- modules/ApiFunction.php
+++ modules/ApiFunction.php
@@ -131,6 +131,35 @@ class ApiFunction
         return "XFUKEY".rand(1000,9999).time().$salts;
     }
 
+    // 生成新user_ssid
+    /**
+     * @param $long_salt
+     * @return string
+     */
+    public function ssid_create($long_salt): string {
+        $salts = null;
+        for ($number = 1;$number <= $long_salt; $number++) {
+            if (time()%2 == 0) {
+                if ($number % 2 == 0 or $number % 3 == 0) {
+                    $salt[$number] = chr(rand(97, 122));
+                } else {
+                    $salt[$number] = rand(0, 9);
+                }
+            } else {
+                if ($number % 2 != 0 or $number % 3 != 0) {
+                    $salt[$number] = chr(rand(97, 122));
+                } else {
+                    $salt[$number] = rand(0, 9);
+                }
+            }
+        }
+        for ($number = 1;$number <= $long_salt; $number++) {
+            $salts = $salts.$salt[$number];
+        }
+        // 定义 ukey
+        return "XFSSID".rand(1000,9999).time().$salts;
+    }
+
     // 生成新CODE
     // type分三种
         // number -> 纯数字验证码
