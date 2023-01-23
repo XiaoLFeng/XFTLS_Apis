fbde1ee821fad46a7d70de642fb8ed4b29a49f5d
XiaoLFeng <gm@x-lf.cn>
2023-01-23 18:25:33 +0800

    修正跨域

--- header-control.php
+++ header-control.php
@@ -9,7 +9,6 @@
 
 // 设置请求头
 header('Content-Type: application/json;charset=utf-8');
-header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
 header('Access-Control-Allow-Methods:POST,GET,OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with, Origin');
