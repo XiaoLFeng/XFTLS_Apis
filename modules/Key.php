7ae82080e93b3423abe5e4e9261212fea65c0e97
XiaoLFeng <gm@x-lf.cn>
2023-01-29 15:52:43 +0800

    添加密钥

--- modules/Key.php
+++ modules/Key.php
@@ -59,4 +59,28 @@ class Key
         // 定义 ukey
         return "XFSSID".rand(1000,9999).time().$salts;
     }
+
+    public function service_acgurl_access_key($long_salt) {
+        $salts = null;
+        for ($number = 1;$number <= $long_salt; $number++) {
+            if (time()%2 == 0) {
+                if ($number % 2 == 0 or $number % 3 == 0) {
+                    $salt[$number] = chr(rand(97, 122));
+                } else {
+                    $salt[$number] = rand(0, 9);
+                }
+            } else {
+                if ($number % 2 != 0 or $number % 3 != 0) {
+                    $salt[$number] = chr(rand(97, 122));
+                } else {
+                    $salt[$number] = rand(0, 9);
+                }
+            }
+        }
+        for ($number = 1;$number <= $long_salt; $number++) {
+            $salts = $salts.$salt[$number];
+        }
+        // 定义 ukey
+        return "XFACG".time().$salts;
+    }
 }
